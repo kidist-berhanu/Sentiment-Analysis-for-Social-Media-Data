f4243905578f3eb8446aead4ffc0547298ff9f2e
merryyainu <129627714+merryyainu@users.noreply.github.com>
2024-03-29 00:01:06 +0300

    Update splitting the dataset.py

--- splitting the dataset.py
+++ splitting the dataset.py
@@ -23,3 +23,5 @@ labels = df['sentiment']
 
 # (80% for training, 20% for testing)
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+train_data=train_data.apply(lambda features:features.split())
+test_data=test_data.apply(lambda labels:comment.split())

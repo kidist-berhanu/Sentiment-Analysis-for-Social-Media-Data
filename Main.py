2f96ca0d46a2b1b9813385c012328ead0a8139fc
meyab <yabsrayabsra2222@gmail.com>
2024-03-28 21:47:01 +0300

    insert the data set

--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+
+# Specify the file path
+file_path = r'C:\Users\Yabsra\Desktop\Projects\Selected topics\Sentiment-Analysis-for-Social-Media-Data\Gaming_comments_sentiments_from_Reddit(Dataset).csv'
+
+# Read the CSV file into a Pandas DataFrame
+pd.read_csv(file_path)
+

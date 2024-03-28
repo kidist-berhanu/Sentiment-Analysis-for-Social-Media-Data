1b8fdccdca3620741dadbb5c5d947007903b84ab
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-03-28 23:08:59 +0300

    Update Data Cleaning.py
    Directory editing

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-file_path = r'C:\Users\Yabsra\Desktop\Projects\Selected topics\Sentiment-Analysis-for-Social-Media-Data\Gaming_comments_sentiments_from_Reddit(Dataset).csv'
+file_path = r'Gaming_comments_sentiments_from_Reddit(Dataset).csv'
 
 df = pd.read_csv(file_path)
 

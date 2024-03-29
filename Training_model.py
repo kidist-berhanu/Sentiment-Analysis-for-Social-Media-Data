28abc31d170e4570f8eb8eb4e65c077b3f359151
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-03-29 23:44:59 +0300

    Update Training_model.py
    Evaluating model

--- Training_model.py
+++ Training_model.py
@@ -74,3 +74,13 @@ X_test =  vectorizer.transform(X_test)
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train,Y_train)
 
+# Model Evaluation 
+# Accuracy score on training data
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(Y_train, X_train_prediction)
+print(training_data_accuracy)
+
+# Accuracy score on test data
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(Y_test, X_test_prediction)
+print(test_data_accuracy)

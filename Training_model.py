7aad9e0b63910e8cc3472b201879212cc8705073
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-03-29 23:16:25 +0300

    Update Training_model.py
    Model Evaluation

--- Training_model.py
+++ Training_model.py
@@ -2,3 +2,9 @@
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train,Y_train)
 
+# Model Evaluation 
+# Accuracy score on training data
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(Y_train, X_train_prediction)
+print(training_data_accuracy)
+

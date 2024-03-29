8611152b2198051413ccb62e7b347ed18922cfd6
Yabsra Yihenew <139139885+Yabsra-Yihenew@users.noreply.github.com>
2024-03-29 22:17:50 +0300

    Update Data Cleaning.py
    Stemming the data

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -30,3 +30,14 @@ df['sentiment'] = df['sentiment'].map(sentiment_mapping)
 
 # Removing duplicates
 df.drop_duplicates(inplace=True)
+
+# Stemming the data
+port_stem = PorterStemmer()
+
+def stemming(content):
+  stemmed_content = re.sub('[^a-zA-Z]',' ',content)
+  stemmed_content = stemmed_content.lower()
+  stemmed_content = stemmed_content.split()
+  stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+  stemmed_content = ' '.join(stemmed_content)
+  return stemmed_content
